467e391df9830aec94da25e7fb3fce35daa45bdb
Arthur Eiko <arthureiko@MacBook-Pro-de-Arthur.local>
2022-06-13 15:58:38 -0300

    first commit

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b0c62d6ce295a0",
+    pass: "1538418ed8d946",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Arthur Eiko <arthureiko29@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
